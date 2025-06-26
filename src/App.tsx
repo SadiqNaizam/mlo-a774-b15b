47764e0f510abff3d73867bc7dfa2474bb926e30
SadiqNaizam <sadiq.naizam1234@gmail.com>
2025-06-27 02:50:43 +0530

    Fixed build errors: App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,7 @@
-import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-
 
 import ForgotPasswordPage from "./pages/ForgotPasswordPage";
 import LoginPage from "./pages/LoginPage";
@@ -14,16 +12,12 @@ import NotFound from "./pages/NotFound";
 
 const queryClient = new QueryClient();
 
-
 const App = () => (
 <QueryClientProvider client={queryClient}>
     <TooltipProvider>
-    <Toaster />
     <Sonner />
     <BrowserRouter>
         <Routes>
-
-
           <Route path="/" element={<LoginPage />} />
           <Route path="/forgot-password" element={<ForgotPasswordPage />} />
           <Route path="/reset-password" element={<ResetPasswordPage />} />
@@ -31,8 +25,6 @@ const App = () => (
           <Route path="/user-dashboard" element={<UserDashboardPage />} />
           {/* catch-all */}
           <Route path="*" element={<NotFound />} />
-
-
         </Routes>
     </BrowserRouter>
     </TooltipProvider>
