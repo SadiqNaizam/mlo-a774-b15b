ee1203117204ca6e56ce7d03110479e1abd5ba01
SadiqNaizam <sadiq.naizam1234@gmail.com>
2025-06-27 02:52:05 +0530

    Fixed build errors: ForgotPasswordPage.tsx

--- src/pages/ForgotPasswordPage.tsx
+++ src/pages/ForgotPasswordPage.tsx
@@ -3,7 +3,7 @@ import { useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
 import * as z from 'zod';
 import { Link, useNavigate } from 'react-router-dom';
-import { toast } from "sonner";
+import { toast } from 'sonner';
 
 // Custom Layout Components
 import Header from '@/components/layout/Header';
